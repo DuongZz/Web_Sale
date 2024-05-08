912b85b85d13b56b3528a7b1abf3630988991b86
duongdaoq <duongdaoq@gmail.com>
2024-05-08 14:54:56 +0700

    delete example

--- src/server.js
+++ src/server.js
@@ -4,10 +4,10 @@ import { DBConnection } from "./config/mongodb";
 const app = express();
 dotenv.config();
 
-DBConnection();
 const PORT = process.env.PORT || 6969;
 app.listen(PORT, () => {
   if (process.env.NODE_ENV === "development") {
     console.log("Listening on port " + PORT + " ❤️");
   }
 });
+DBConnection();
