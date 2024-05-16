6e2e3156a4c2773316332a2f321d7d4499039880
Horob1 <nguyentheanh24102003@gmail.com>
2024-05-16 19:44:18 +0700

    config all cors and cookie

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import router from "./routes";
 import morgan from "morgan";
 import { errorHandlingMiddleware } from "./middlewares/errorHandlingMiddleware";
+import { env } from "./config/environment";
 
 const app = express();
+
+app.use(cors(
+  {
+    origin: [env.REACT_GEARVN_CLONE_HOST,],
+    credentials: true
+  }
+))
 
 app.use(express.json());
 
