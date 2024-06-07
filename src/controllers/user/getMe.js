450adf6589de084289972010694faff123aabc77
Horob1 <nguyentheanh24102003@gmail.com>
2024-06-08 00:01:51 +0700

    product complete

--- src/controllers/user/getMe.js
+++ src/controllers/user/getMe.js
@@ -3,18 +3,17 @@ import { findUserById } from "~/models/userModel";
 
 export const getMe = async (req, res, next) => {
   try {
-    
-    if(req.user) {
-      const user = await findUserById(req.user.id)
+    if (req.user) {
+      const user = await findUserById(req.user.id);
 
-      delete user.refreshToken
+      if (user?.refreshToken) delete user.refreshToken;
 
       res.status(StatusCodes.OK).json({
-        message: 'Get user information successfully',
-        user
-      })
+        message: "Get user information successfully",
+        user,
+      });
     }
   } catch (error) {
     next(error);
   }
-}
+};
