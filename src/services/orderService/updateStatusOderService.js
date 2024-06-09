08b85632a44cc6cc774f1bafc2fe3c84e117b4c9
duongdaoq <duongdaoq@gmail.com>
2024-06-10 00:09:03 +0700

    update status order

--- src/services/orderService/updateStatusOderService.js
+++ src/services/orderService/updateStatusOderService.js
@@ -1,17 +1,26 @@
 import { findOrderById, updateOrder } from "~/models/orderModel";
 import { StatusCodes } from "http-status-codes";
 import ApiError from "~/utils/ApiError";
+import { returnProduct } from "~/models/productModel";
 
 export const updateStatusOrderService = async (req, res, next) => {
   try {
-    const id = req.body.id;
+    const { id, status } = req.body;
     const order = await findOrderById(id);
-    if (!order) next(new ApiError(StatusCodes.NOT_FOUND, "Not Found Order"));
+    console.log("🚀 ~ updateStatusOrderService ~ order:", order);
+    if (!order) throw new ApiError(StatusCodes.NOT_FOUND, "Not Found Order");
 
     await updateOrder(id, {
-      status: req.body.status,
+      status: status,
       updateAt: new Date(),
     });
+    // Kiểm tra nếu trạng thái mới là "rejected", sau đó tăng số lượng tồn kho
+    if (status === "rejected") {
+      console.log(
+        "🚀 ~ updateStatusOrderService ~ order.product.quantity:",
+        order.product
+      );
+    }
   } catch (error) {
     throw error;
   }
