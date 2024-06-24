4a2323ceb6a8f0397a2b8ec7e551443fdac055cc
Horob1 <nguyentheanh24102003@gmail.com>
2024-06-24 08:49:42 +0700

    fix oauth

--- src/controllers/auth/oauthGoogle.js
+++ src/controllers/auth/oauthGoogle.js
@@ -1,56 +1,57 @@
 import { StatusCodes } from "http-status-codes";
 import { env } from "~/config/environment";
 import { createUser, findUserByEmail, updateUser } from "~/models/userModel";
-import { getGoogleOauthToken, getGoogleUser } from "~/services/authService/userService";
+import {
+  getGoogleOauthToken,
+  getGoogleUser,
+} from "~/services/authService/userService";
 import ApiError from "~/utils/ApiError";
-import { generateAccessToken, generateRefreshToken } from "~/utils/generateToken";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+} from "~/utils/generateToken";
 
 export const oauthGoogle = async (req, res, next) => {
   try {
     const code = req.query.code;
 
-    const {id_token, access_token} = await getGoogleOauthToken(code)
+    const { id_token, access_token } = await getGoogleOauthToken(code);
 
-    const googleUser = await getGoogleUser(id_token, access_token)
+    const googleUser = await getGoogleUser(id_token, access_token);
 
     if (!googleUser.verified_email) {
-      return next(new ApiError(StatusCodes.UNAUTHORIZED, "Google account is not verified"))
+      return next(
+        new ApiError(StatusCodes.UNAUTHORIZED, "Google account is not verified")
+      );
     }
 
-    let user =  await findUserByEmail(googleUser.email)
-    if(!user) {
-      user = await createUser({email: googleUser.email, name: "Google User", password: "defautpassword", role: "user"})
+    let user = await findUserByEmail(googleUser.email);
+    if (!user) {
+      user = await createUser({
+        email: googleUser.email,
+        name: "Google User",
+        password: "defautpassword",
+        role: "user",
+      });
     }
-    
+
     const accessToken = generateAccessToken(user);
     const refreshToken = generateRefreshToken(user);
 
-      await updateUser({_id: user.insertedId},{
+    await updateUser(
+      { _id: user.insertedId },
+      {
         $set: {
           refreshToken: refreshToken,
         },
-      }, {})
+      },
+      {}
+    );
 
-      res.cookie("refreshToken", refreshToken, {
-        path: "/",
-        sameSite: "None",
-        secure: env.COOKIE_IS_USE_SECURE,
-        httpOnly: true,
-        partitioned: true,
-      });
-
-      res.cookie("accessToken", accessToken, {
-        path: "/",
-        sameSite: "None",
-        secure: env.COOKIE_IS_USE_SECURE,
-        httpOnly: true,
-        partitioned: true,
-      });
-
-      res.redirect(`${env.REACT_GEARVN_CLONE_HOST}`)
-      
-
+    res.send(
+      `<script>window.opener.postMessage({ accessToken: "${accessToken}", refreshToken: "${refreshToken}"  }, "*");window.close();</script>`
+    );
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
